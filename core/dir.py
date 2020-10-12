2a98c1f4d9654c8b2cd6f016f01d1366f0548f94
test <MY_NAME@example.com>
2020-10-12 15:36:06 +0200

    add ifHasSeries for validation

--- core/dir.py
+++ core/dir.py
@@ -1,26 +1,37 @@
 import re
 
-class ifStar:
-    dir = 'test (dqdwqwd and qdqwd)'
-    def ifHasAstar(self):
-        x=re.search("[a-zA-Z0-9]+\s+\([a-zA-Z0-9\s]+\)", self.dir)
-        if x:
+class ifHasSeries:
+    dir = '[feqfqef] qfqefjqepfjqepfj';
+    def ifHasSeries(self):
+        if re.search("\[[a-zA-Z0-9\s]+\]\s[a-zA-Z0-9]+", self.dir):
             return True
         return False
     def faindElment(self):
-        if self.ifHasAstar():
-            print('fqefeqf')
+        if self.ifHasSeries():
+            print('has a series')
+    def validate(self):
+        self.faindElment()
+
+class ifStar:
+    dir = 'test (dqdwqwd and qdqwd)'
+    def ifHasAStar(self):
+        if re.search("[a-zA-Z0-9]+\s+\([a-zA-Z0-9\s]+\)", self.dir):
+            return True
+        return False
+    def faindElment(self):
+        if self.ifHasAStar():
+            print('has a star')
 
     def validate(self):
-        self.ifHasAstar()
         self.faindElment()
 
 class manageDir:
     dir = 'test (dqdwqwd qdqwd)'
     def __init__(self):
         self.ifStar=ifStar()
+        self.ifHasSeries = ifHasSeries()
     def set(self):
         self.runValidate()
     def runValidate(self):
         self.ifStar.validate()
-
+        self.ifHasSeries.validate()
