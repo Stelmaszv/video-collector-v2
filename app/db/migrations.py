652e8ca01de46b0440ef89cf37ec310b4cc926a5
test <MY_NAME@example.com>
2020-10-14 14:10:57 +0200

    add migrations in migrations.py

---
+++ app/db/migrations.py
@@ -0,0 +1,37 @@
+from core.db.config import cur,con
+
+cur.execute("DROP TABLE IF EXISTS series;")
+cur.execute("DROP TABLE IF EXISTS movies;")
+cur.execute("DROP TABLE IF EXISTS stars;")
+cur.execute("DROP TABLE IF EXISTS starsinmovies;")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS stars (
+        id INTEGER PRIMARY KEY ASC,
+        name varchar(250) NOT NULL
+    )""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS series (
+        id INTEGER PRIMARY KEY ASC,
+        name varchar(250) NOT NULL
+    )""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS movies (
+        id INTEGER PRIMARY KEY ASC,
+        name varchar(250) NOT NULL,
+        series INTEGER NOT NULL, 
+        FOREIGN KEY(series) REFERENCES series(id)
+    )""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS starsinmovies (
+        id INTEGER PRIMARY KEY ASC,
+        movie INTEGER NOT NULL,
+        star INTEGER NOT NULL, 
+        FOREIGN KEY(movie) REFERENCES movies(id),
+        FOREIGN KEY(star)  REFERENCES stars(id)
+    )""")
+
+con.commit()
