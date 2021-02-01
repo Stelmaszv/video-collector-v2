2bc4c44dbb597515d65299e1d70f0a1ab4ca98c4
test <MY_NAME@example.com>
2021-02-01 13:11:21 +0100

    Update in class "AddTagView" add method "init"

--- view/tags/add_tags.py
+++ view/tags/add_tags.py
@@ -1,6 +1,11 @@
 from PyQt5.QtWidgets import QWidget
+from core.view import BaseView
 class AddTagView(QWidget):
 
     def run_window(self):
+        self.init()
         self.show()
-        return True
+
+    def init(self):
+        self.model=self.obj.model
+        self.BaseView = BaseView([], self)
