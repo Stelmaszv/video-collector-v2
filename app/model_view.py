6f3c3402e008294275138f6055154b06f0233675
test <MY_NAME@example.com>
2021-03-12 15:34:20 +0100

    Add class "MoviesModelView"

--- app/model_view.py
+++ app/model_view.py
@@ -4,6 +4,8 @@ from app.db.models import session
 class BaseModelViewSet:
     pass
 
+
+
 class SetPhotoToSetiesView(BaseModelViewSet):
 
     model = Series()
@@ -17,6 +19,32 @@ class SetPhotoToSetiesView(BaseModelViewSet):
         for sezon in self.data.sezons:
             sezon.src=data[item]['value']
             item=item+1
+
+class MoviesModelView(BaseModelViewSet):
+    model = Series()
+    session = session
+
+    def __init__(self,data):
+        self.data=data
+
+    def add_data(self, data):
+        if data[0]['value']:
+            self.data.name = data[0]['value']
+
+        if data[1]['value']:
+            self.data.country = data[1]['value']
+
+        if data[2]['value']:
+            self.data.year = data[2]['value']
+
+        if data[3]['value']:
+            self.data.dir = data[3]['value']
+
+        if data[4]['value']:
+            self.data.avatar = data[4]['value']
+
+        self.session.add_all([self.data])
+        self.session.commit()
 
 class SeriesModelView(BaseModelViewSet):
     model = Series()
--- view/movie/edit_movie.py
+++ view/movie/edit_movie.py
@@ -2,14 +2,14 @@ from PyQt5.QtWidgets import QWidget
 from app.db.models import Movies
 from core.view import AbstractBaseView
 from app.forms import MovieEditForm
-from app.model_view import StarModelView
+from app.model_view import MoviesModelView
 
 class EditMovieView(QWidget,AbstractBaseView):
     model = Movies
     resolution_index = 'EditSeries'
     submit_view = 'movies'
     FormSchema = MovieEditForm
-    ModelView = StarModelView
+    ModelView = MoviesModelView
     show_elemnts = ['Info', 'Galery', 'Nav', 'Avatar','List','Description','Tags']
     methods = ['add_star','add_tag', 'submit_click']
 
@@ -18,7 +18,7 @@ class EditMovieView(QWidget,AbstractBaseView):
 
     def submit_click(self,values):
         self.Submit.set_data(values)
-        #self.Submit.run()
+        self.Submit.run()
 
     def add_star(self,values):
         self.BaseView.load_view('movie_add_star_to_model_view', self.data)
