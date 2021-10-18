dbd30b93a20a7a3ca182a2f30f49e28791d3382b
= <stelmaszv@gmail.com>
2021-10-18 15:39:00 +0200

    Add methods "return_stars" in class "CreateJSONDBLIST" (create_JSON_DB_LIST_DB.py)

--- core/create_JSON_DB_LIST.py
+++ core/create_JSON_DB_LIST.py
@@ -4,30 +4,41 @@ import json
 
 class CreateJSONDBLIST:
     Sesion = session
+    series_fields = ["years", "country", "number_of_sezons", "movies", "stars"]
+    stars_fields = ['weight', 'height', 'ethnicity', 'hair_color', 'date_of_birth']
+    movies_fields = []
 
     def base_get(self, Model, atters):
+
+        if hasattr(Model, "__len__"):
+            loop_all = Model
+        else:
+            loop = self.loop(Model)
+            loop_all = loop.all()
         array_return = []
-        loop = self.loop(Model)
-        for item_db in loop.all():
+        for item_db in loop_all:
             item = self.add_defults(item_db)
             for atter in atters:
                 if atter != "series":
                     if hasattr(item_db, atter):
                         item[atter] = getattr(item_db, atter)
-                    array_return.append(item)
                 if atter == "series":
                     item["series"] = self.return_series(item_db)
+                if atter == "movies":
+                    item["movies"] = self.return_movies(item_db)
+                if atter == "stars":
+                    item["stars"] = self.return_stars(item_db)
+            array_return.append(item)
         return array_return
 
+    def return_stars(self, item):
+        return self.base_get(item.stars, self.stars_fields)
+
+    def return_movies(self, item):
+        return self.base_get(item.movies, self.movies_fields)
+
     def return_series(self, item):
-        series = []
-        for item_db in item.series:
-            item = self.add_defults(item_db)
-            item["years"] = getattr(item_db, "years")
-            item["country"] = getattr(item_db, "country")
-            item['number_of_sezons'] = getattr(item_db, "number_of_sezons")
-            series.append(item)
-        return series
+        return self.base_get(item.series, self.series_fields)
 
     def return_tags(self, tags):
         tags_array = []
@@ -64,7 +75,7 @@ class CreateJSONDBLIST:
         return {}
 
     def get_stars(self):
-        return self.base_get(Stars, ['weight', 'height', 'ethnicity', 'hair_color', 'date_of_birth'])
+        return self.base_get(Stars, self.stars_fields)
 
     def create(self):
         list = []
