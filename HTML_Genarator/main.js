64c7cdaf370e9c572bab6a1371d8087424f26383
= <stelmaszv@gmail.com>
2021-10-19 18:45:03 +0200

    Update in main.js and add "products_id.html"

--- HTML_Genarator/main.js
+++ HTML_Genarator/main.js
@@ -1,4 +1,4 @@
-(function LoadProduent(){
+function LoadProduent(){
     function add_Series(series_json){
         series = '<ul>';
         for (let serie of series_json){
@@ -15,4 +15,36 @@
         list.innerHTML += add_Series(el.series)
         list.innerHTML += '</div>';
     }
-})();
+};
+
+function LoadProduentID(){
+    function add_Series(series_json){
+        series = '<ul>';
+        for (let serie of series_json){
+            series += '<li><a href="'+serie.dir+'/index.html">'+serie.name+'</a> </li>'
+        }
+        series += '</ul>';
+        return series
+    }
+    function add_movies(series_json){
+        series = '<ul>';
+        for (let serie of series_json){
+            series += '<li><a href="'+serie.dir+'/index.html">'+serie.name+'</a> </li>'
+        }
+        series += '</ul>';
+        return series
+    }
+    setTimeout(function(){ 
+        let name = document.querySelector(".name"); 
+        name.innerHTML = data.name;
+        let series_lists = document.querySelector(".series_lists"); 
+        series_lists.innerHTML = add_Series(data.series);
+        let movies_lists = document.querySelector(".movies_lists"); 
+        movies_lists.innerHTML = add_movies(data.series);
+        let head = document.querySelector("title"); 
+        head.innerHTML = data.name; 
+    }, 10);
+
+    
+
+}
