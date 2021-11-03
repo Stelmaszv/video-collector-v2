ea08a610c316834563f775f5c91bb55fae6597e8
= <stelmaszv@gmail.com>
2021-11-03 17:30:02 +0100

    Fixed pagination "movies.html"

--- HTML_Genarator/js/load.js
+++ HTML_Genarator/js/load.js
@@ -8,21 +8,15 @@ class LoadContet{
         let div_output=document.querySelector(this.div_output)
         div_output.innerHTML=''
         this.ListOBJ = this.set_obj()
-        this.ListOBJ.return_data(page)
-        console.log(filter)
         if (this.filter_is_not_empty(filter)){
-            page=0
-            div_output.innerHTML=''
             this.ListOBJ.return_data_filter(filter,this.data)
             movies_results=this.data
             this.data=this.ListOBJ.array
             this.set_list()
             this.ListOBJ = this.set_obj()
-            console.log(this.pagineted_data)
-            this.ListOBJ.return_data(page)
             movies_results=this.ListOBJ.array
         }
-    
+        this.ListOBJ.return_data(page)
         page++
     }
 
