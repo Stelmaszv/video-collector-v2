0297526d962674ced189ce145a42c54f5ad64448
= <stelmaszv@gmail.com>
2021-12-01 12:38:59 +0100

    Update in method "start_scan" (movie_scan.py)

--- view/VCSCAN/movie_scan.py
+++ view/VCSCAN/movie_scan.py
@@ -9,18 +9,24 @@ class MovieScanInfo(QWidget, AbstractBaseView):
     window_title = 'Found Items to add!'
     resolution_index = 'VCSCAN'
     show_elemnts = ['Title', 'Galery', 'Nav', 'Avatar', 'List', 'Form']
-
+    adding=False
     def after_init(self):
-        data = {
-            "click_btm_info": "Star Scan!",
-            "res":self.WindowSize['custum_button'],
-            "click_method":self.start_scan,
-            "font": ['Times',50]
-        }
-        self.BaseView.Form.custum_button(data)
+        if self.adding is False:
+            data = {
+                "click_btm_info": "Star Scan!",
+                "res":self.WindowSize['custum_button'],
+                "click_method":self.start_scan,
+                "font": ['Times',50]
+            }
+            self.BaseView.Form.custum_button(data)
 
     def start_scan(self):
-        print('Scaning')
+        self.close()
+        Movie_scan=MovieScanInfo()
+        Movie_scan.show_elemnts.append('Info')
+        Movie_scan.window_title='Adding objects ... '
+        Movie_scan.adding = True
+        Movie_scan.run_window()
 
 
 
