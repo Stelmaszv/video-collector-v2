c39aa647bf6d4f39b6fd25a4acddfc29933d58b9
= <stelmaszv@gmail.com>
2022-02-07 15:14:59 +0100

    Fix dir in web admin

--- HTML_Genarator/js/counter_data.js
+++ HTML_Genarator/js/counter_data.js
@@ -1,4 +1,4 @@
-const movies_count = 22 
+const movies_count = 23 
 const series_count = 2 
 const stars_count = 25 
 const producents_count = 0 
--- HTML_Genarator/js/movies_list.js
+++ HTML_Genarator/js/movies_list.js
@@ -1,2 +1,2 @@
 var data = ['Z:/HTML Generator/js/movies/01.js', 'Z:/HTML Generator/js/movies/02.js', 'Z:/HTML Generator/js/movies/03.js'] 
-var data_count = 22
+var data_count = 23
